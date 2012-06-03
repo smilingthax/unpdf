0af59ec75d42e4e4aed799d615d4b332bb6d1e53
smilingthax <smilingthax@8e93851d-d448-0410-a3eb-83dfef38a4c3>
2012-06-03 22:49:14 +0000

    added 1.7 supplement (ExtensionLevel 3) crypto

--- unpdf/pdfcrypt.h
+++ unpdf/pdfcrypt.h
@@ -24,6 +24,23 @@ namespace PDFTools {
     MD5(const MD5 &);
     const MD5 &operator=(const MD5 &);
   };
+  class SHA256 {
+  public:
+    SHA256();
+    ~SHA256();
+    void init();
+    void update(const char *data,int len);
+    void update(const std::string &str);
+    void finish(char *hash); // >hash to be at least 32 bytes
+
+    static void sha256(char *hash,const char *data,int len); // allowed: hash==data
+  private:
+    class SHA256_impl;
+    SHA256_impl *impl;
+
+    SHA256(const SHA256 &);
+    const SHA256 &operator=(const SHA256 &);
+  };
   class RC4 {
   public:
     RC4();
@@ -46,7 +63,7 @@ namespace PDFTools {
     AESCBC(); // default: encrypt
     AESCBC(const std::string &key,bool encrypt);
     ~AESCBC();
-    // 16 byte >key
+    // 16 or 32 byte >key
     void setkey(const char *key,int len,bool encrypt);
 
     // 16 byte >iv, will be updated,  >len to be multiple of 16
--- unpdf/pdfsec.h
+++ unpdf/pdfsec.h
@@ -47,12 +47,13 @@ namespace PDFTools {
     enum CFMode { StmF=0, StrF, Eff };
     Decrypt *getDecrypt(const Ref &ref,CFMode cfm);
 
-    bool check_userpw(const std::string &user_pw);
-    bool check_ownerpw(const std::string &owner_pw);
+    bool check_pw(const std::string &pw); // TODO: return which one
 
+// TODO? static:
     // for writing 
     std::pair<std::string,std::string> set_pw(const std::string &owner_pw,const std::string &user_pw); // (owner_hash,user_hash) // TODO
     std::pair<std::string,std::string> set_pw(const std::string &user_pw);
+    Dict *set_pw389(const std::string &owner_pw,const std::string &user_pw,int perms,bool encmeta);
   protected:
     void parse_stdcf(PDF &pdf,const Object *obj);
     static const char pad[];
@@ -62,6 +63,13 @@ namespace PDFTools {
     std::string compute_owner_key(const std::string &owner_pw);
     std::string encrypt_owner_hash(const std::string &owner_key,const std::string &user_pw);
     std::string decrypt_owner_hash(const std::string &owner_key,const std::string &owner_hash);
+    bool check_userpw(const std::string &user_pw);
+    bool check_ownerpw(const std::string &owner_pw);
+    bool check_ownerpw32a(const std::string &owner_pw);
+    bool check_userpw32a(const std::string &user_pw);
+    std::pair<std::string,std::string> set_userpw38(const std::string &key,const std::string &user_pw); // (/U,/UE)
+    std::pair<std::string,std::string> set_ownerpw39(const std::string &key,const std::string &u_hash,const std::string &owner_pw); // (/O,/OE)
+    void validate_perm();
   private:
     Dict dict;
     int algo,len;
@@ -69,16 +77,18 @@ namespace PDFTools {
     std::string docid;
     std::string ownerpw_hash,userpw_hash;
     bool encryptMeta;
+    // new in 1.7 Supp.3
+    std::string ownerpw_key,userpw_key,perm_enc;
 
     struct CryptFilter {
       CryptFilter() : method(None),event(DocOpen),len(-1) {}
-      enum Method { None, V2, AESV2 } method;
-      enum AuthEvent { DocOpen, EFOpen } event;
+      enum Method { None, V2, AESV2, AESV3 } method;
+      enum AuthEvent { DocOpen, EFOpen } event;  // 1.7 Supp.3: only DocOpen
       int len; // len==-1: /Identity
     };
     CryptFilter stdcf,modecf[3];
 
-    std::string ownerpw,userpw; // stores userinput
+    std::string ownerpw,userpw; // stores userinput (ownerpw empty if only userpw known)
     std::string filekey; // will be computed; !empty if valid pw known
   };
 };
